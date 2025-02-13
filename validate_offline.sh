9a0a55e02ecf24f0f636231e834c069d661e5c33
grigorimaxim422 <grigorimaxim422@gmail.com>
2025-02-13 23:50:27 +0000

    sbash fixed

--- validate_offline.sh
+++ validate_offline.sh
@@ -1,3 +1,11 @@
-python3 -m validate.offline_vali_trainer --epochs 50 --model_path  "saved_model/model.pt" --learning_rate 0.025
+python3 -m validate.offline_vali_trainer --validate_epochs 50  --model_path  "saved_model/efficient01A.pt.best" --learning_rate 0.025
 
+# python3 -m validate.offline_vali_trainer --epochs 50 --model_path  "saved_model/model.pt" --learning_rate 0.025
 # python3 -m validate.offline_vali_trainer --epochs 50 --model_path  "saved_model/first.pt" --learning_rate 0.025
+
+##Testing
+#python3 -m validate.offline_vali_trainer --validate_epochs 5  --model_path  "saved_model/mobilenetv2.pt" --learning_rate 0.025
+#python3 -m validate.offline_vali_trainer --validate_epochs 50  --model_path  "saved_model/efficient01.pt.best" --learning_rate 0.025 #61%, 59%,
+
+#python3 -m validate.offline_vali_trainer --validate_epochs 50  --model_path  "saved_model/nsgac100.pt" --learning_rate 0.025
+
